fc9dd11b9f536b2e3219ec22d799e0e5d654abbd
yash-0025 <yashpatel200403@gmail.com>
2025-07-23 16:24:45 +0530

    VARIABLES FUNCTIONS

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let ans = is_even(10);
     println!("{}", ans);
+    println!("{}", fib(10));
+
+    let name = String::from("Yash");
+    let len = get_str_ln(name);
+    println!("The length of the string is {}", len);
 }
 
 
@@ -10,4 +15,29 @@ fn is_even(num: i32) -> bool {
     } else {
         return false;
     }
+}
+
+fn fib(num: u32) -> u32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    if num == 0 {
+        return first;
+    }
+
+    if num == 1 {
+        return second;
+    }
+
+    for _ in 0..(num - 1) {
+        let temp = second;
+        second = second + first;
+        first = temp;
+    }
+    return second;
+}
+
+
+fn get_str_ln(str: String) -> usize {
+    str.chars().count()
 }
