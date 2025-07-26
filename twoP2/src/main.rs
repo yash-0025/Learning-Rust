0232caad31748020e690764500bec3d6d19856a7
yash-0025 <yashpatel200403@gmail.com>
2025-07-26 17:20:08 +0530

    STRINGS AND SLICES

---
+++ twoP2/src/main.rs
@@ -0,0 +1,63 @@
+// STRINGS VS SLICES
+/* 
+The string variable will be stored on stack and it will reference to the heap where the actual string data is located 
+suppose there is a string 
+        let s = String::from("yash");
+here the variable s will be stored on stack and the data hello will be stored on heap . The stack structure is like this :: 
+name - value
+ptr - poiting to heap index
+len - 4
+capacity - 4        
+# Heap will look like this :: 
+index - value
+0 - Y
+1 - A
+2 - S
+3 - H
+
+1. Creating a string
+2. Mutating a string
+3. Deleting from a string
+fn main() {
+let name = String::from("yash");
+let mut name = String::from("yash");
+name.push_str(" patel")
+println!("Name is :: {}", name);
+name.replace_range(8..name.len(), "");
+println!("name is {}", name); 
+}
+*/
+
+fn main() {
+    let name = String::from("hello world");
+    let ans = first_word(str:name);
+    println!("Ans is {}", ans);
+
+// STRING SLICE 
+    let mut word = String::from("Hello World");
+    let word2 = find_first_word(&word);
+    println!("{}", word);
+}
+
+fn find_first_word(word: &String) -> &str {
+    let mut index = 0;
+    for (_,i) in word.chars().enumerate() {
+        if i == ' ' {
+            break;
+        }
+        index = index + 1;
+    }
+
+    return &word[0..index];
+}
+
+fn first_word(str: String) -> String {
+    let mut ans = String::from("");
+    for i in str.chars() {
+        if i == ' ' {
+            break;
+        }
+        ans.push_str(&i.to_string());
+    }
+    return ans;
+}
---
+++ twoP2/target/debug/deps/twoP2-24fd80750aa69bd5.d
@@ -0,0 +1,5 @@
+/mnt/d/Smart Contract Auditing/Rust/Marathon/twoP2/target/debug/deps/twoP2-24fd80750aa69bd5: src/main.rs
+
+/mnt/d/Smart Contract Auditing/Rust/Marathon/twoP2/target/debug/deps/twoP2-24fd80750aa69bd5.d: src/main.rs
+
+src/main.rs:
