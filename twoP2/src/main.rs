b03fd5510f166d55338e0efe4f74617bc3636ce6
yash-0025 <yashpatel200403@gmail.com>
2025-07-26 17:29:19 +0530

    GENERICS

--- twoP2/src/main.rs
+++ twoP2/src/main.rs
@@ -28,6 +28,10 @@ println!("name is {}", name);
 }
 */
 
+// GENERICS 
+/* 
+We can define generics type for the input argument to prevent code repetition or something like the same scenario
+*/
 fn main() {
     let name = String::from("hello world");
     let ans = first_word(str:name);
@@ -37,6 +41,21 @@ fn main() {
     let mut word = String::from("Hello World");
     let word2 = find_first_word(&word);
     println!("{}", word);
+
+
+// GENERICS - here we can use the same function for both number and string because of this generic types functionality otherwise we have to use two different functions 
+    let bigger = largest(1,2);
+    let bigger_char = largest(a,b);
+    println!("Bigger number is :: {}",bigger);
+    println!("Bigger char is :: {}",bigger_char);
+}
+
+fn largest<T: std::cmp::PartialOrd>(a:T, b:T) -> T {
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn find_first_word(word: &String) -> &str {
